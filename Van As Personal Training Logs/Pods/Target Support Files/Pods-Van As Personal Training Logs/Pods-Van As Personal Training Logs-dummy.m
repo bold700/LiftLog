6c00e66042c90f87eea2c3beedad5db014362935
Kenny Timmer <kenny.timmer@frieslandcampina.com>
2025-11-22 11:42:48 +0100

    appstore

---
+++ Van As Personal Training Logs/Pods/Target Support Files/Pods-Van As Personal Training Logs/Pods-Van As Personal Training Logs-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Van_As_Personal_Training_Logs : NSObject
+@end
+@implementation PodsDummy_Pods_Van_As_Personal_Training_Logs
+@end
---
+++ Van As Personal Training Logs/Van As Personal Training Logs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Van As Personal Training Logs
+//
+//  Created by Kenny Timmer on 21/11/2025.
+//
+
+import UIKit
+import Capacitor
+
+class ViewController: CAPBridgeViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
